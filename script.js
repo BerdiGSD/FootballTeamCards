3695c266b778659db4f777c83bf4e5ca8595f63a
BerdiGSD <110266597+BerdiGSD@users.noreply.github.com>
2024-05-01 06:17:31 -0500

    update reset --soft head

--- script.js
+++ script.js
@@ -189,7 +189,7 @@ const setPlayerCards = (arr = players) => {
     playerCards.innerHTML += arr.map(({name, position, number, isCaptain, nickname}) => {
         `
         <div> class="player-card">
-            <h2>${isCaptain === true ? "(Captain)" : "" } ${name}</h2>
+            <h2>${isCaptain === true ? "(Captain)" : ""} ${name}</h2>
         </div>
         `;
     });
